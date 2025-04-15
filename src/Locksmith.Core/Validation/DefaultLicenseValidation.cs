9d5d3e0c730c2b1d4f32c5d66806cad16fd3d5f0
Kerpackie <81828282+Kerpackie@users.noreply.github.com>
2025-04-15 12:56:27 +0100

    Add License Validation

---
+++ src/Locksmith.Core/Validation/DefaultLicenseValidation.cs
@@ -0,0 +1,30 @@
+using Locksmith.Core.Exceptions;
+using Locksmith.Core.Models;
+
+namespace Locksmith.Core.Validation;
+
+public class DefaultLicenseValidation : ILicenseValidator
+{
+    public void Validate(LicenseInfo licenseInfo)
+    {
+        if (licenseInfo == null)
+        {
+            throw new LicenseValidationException("License informaion is missing.");
+        }
+
+        if (string.IsNullOrWhiteSpace(licenseInfo.Name))
+        {
+            throw new LicenseValidationException("Name is required.");
+        }
+        
+        if (string.IsNullOrWhiteSpace(licenseInfo.ProductId))
+        {
+            throw new LicenseValidationException("Product ID is required.");
+        }
+        
+        if (licenseInfo.ExpirationDate.HasValue && licenseInfo.ExpirationDate.Value < DateTime.UtcNow)
+        {
+            throw new LicenseValidationException("Expiration date is in the past.");
+        }
+    }
+}
